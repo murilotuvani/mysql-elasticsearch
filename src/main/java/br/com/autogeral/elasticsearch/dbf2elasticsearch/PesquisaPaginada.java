59ab94337af63c8d4ec6f450ef4fa2fc1bf63483
Murilo Tuvani <murilo.tuvani@gmail.com>
2020-03-20 20:09:50 -0300

    [update] Pesquisa paginada

---
+++ src/main/java/br/com/autogeral/elasticsearch/dbf2elasticsearch/PesquisaPaginada.java
@@ -0,0 +1,78 @@
+package br.com.autogeral.elasticsearch.dbf2elasticsearch;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.http.HttpHost;
+import org.elasticsearch.action.search.ClearScrollRequest;
+import org.elasticsearch.action.search.ClearScrollResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchScrollRequest;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.unit.TimeValue;
+import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
+import org.elasticsearch.search.Scroll;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+
+/**
+ * 20/03/2020 20:01:41
+ *
+ * @see https://www.elastic.co/guide/en/elasticsearch/client/java-rest/master/java-rest-high-search-scroll.html
+ * @author murilo
+ */
+public class PesquisaPaginada {
+
+    private static final String INDEX = "itens";
+    private static final String TYPE = "item";
+
+    private final RestHighLevelClient client = new RestHighLevelClient(
+            RestClient.builder(
+                    new HttpHost("localhost", 9200, "http")
+            ));
+
+    public static void main(String args[]) {
+        PesquisaPaginada s = new PesquisaPaginada();
+        s.pesquisar();
+    }
+
+    private void pesquisar() {
+        try {
+            final Scroll scroll = new Scroll(TimeValue.timeValueMinutes(1L));
+            SearchRequest searchRequest = new SearchRequest(INDEX);
+            searchRequest.scroll(scroll);
+            SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+            searchSourceBuilder.query(matchQuery("descricao", "Golf+Terminal"));
+            searchRequest.source(searchSourceBuilder);
+
+            SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);
+            String scrollId = searchResponse.getScrollId();
+            SearchHit[] searchHits = searchResponse.getHits().getHits();
+
+            while (searchHits != null && searchHits.length > 0) {
+
+                SearchScrollRequest scrollRequest = new SearchScrollRequest(scrollId);
+                scrollRequest.scroll(scroll);
+                searchResponse = client.scroll(scrollRequest, RequestOptions.DEFAULT);
+                scrollId = searchResponse.getScrollId();
+                //searchHits = searchResponse.getHits().getHits();
+                for (SearchHit hit : searchResponse.getHits().getHits()) {
+                    System.out.println("Type     : " + hit.getType());
+                    System.out.println("Document : " + hit.getSourceAsString());
+                }
+
+            }
+
+            ClearScrollRequest clearScrollRequest = new ClearScrollRequest();
+            clearScrollRequest.addScrollId(scrollId);
+            ClearScrollResponse clearScrollResponse = client.clearScroll(clearScrollRequest, RequestOptions.DEFAULT);
+            boolean succeeded = clearScrollResponse.isSucceeded();
+        } catch (IOException ex) {
+            Logger.getLogger(PesquisaPaginada.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+}
